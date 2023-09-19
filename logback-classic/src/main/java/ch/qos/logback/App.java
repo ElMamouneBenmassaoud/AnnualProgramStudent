c68ecac1df4cbd4f5c3167f96a91e34a80c0e93c
Mamoun benmassaoud <mamounbenmassaoud@host-dyn-ip-26-170.wlg.intranet.esi-bru.be>
2023-09-19 12:22:53 +0200

    first commit

---
+++ logback-classic/src/main/java/ch/qos/logback/App.java
@@ -0,0 +1,13 @@
+package ch.qos.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class App {
+
+private static final Logger log = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        log.info("Hello, ");
+        log.error("world !");
+    }
+}
