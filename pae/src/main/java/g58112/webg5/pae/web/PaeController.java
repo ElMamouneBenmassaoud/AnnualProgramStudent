2d2248b7acbe13df65c4bcfcce1e0d694638c735
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-09-26 15:07:17 +0200

    fixer bug raffraichissement

---
+++ pae/src/main/java/g58112/webg5/pae/web/PaeController.java
@@ -0,0 +1,13 @@
+package g58112.webg5.pae.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
