1df447f3e8d65ea2c1ce1b7d7b1f47872c9f47c7
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-10-03 15:32:27 +0200

    validation pas pris en compte (Vérification Mme ARO)

--- pae/src/main/java/g58112/webg5/pae/model/Course.java
+++ pae/src/main/java/g58112/webg5/pae/model/Course.java
@@ -1,11 +1,22 @@
 package g58112.webg5.pae.model;
 
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Course {
+    @NotBlank @NotNull @Size(min=4 , max=10, message = "Le nombre de caractère doit etre compris entre 4 et 10 caractères !") 
+    @Pattern(regexp = "[A-Za-z][A-Za-z0-9]∗",message="Le login est composé de chiffres et de lettres et doit commencer par une lettre.")
     private String id;
+
+    @NotBlank @NotNull @Size(max=100)
     private String title;
+
+    @NotBlank @NotNull @Positive
     private int credits;
 
     public Course(String id, String title, int credits) {
--- pae/src/main/java/g58112/webg5/pae/web/CourseController.java
+++ pae/src/main/java/g58112/webg5/pae/web/CourseController.java
@@ -1,19 +1,33 @@
 package g58112.webg5.pae.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+
 import g58112.webg5.pae.business.PAE;
 import g58112.webg5.pae.model.Course;
+import jakarta.validation.Valid;
 
 @Controller
 public class CourseController {
 
+    @ModelAttribute(name = "courses")
+    public List<Course> courses(Model model) {
+        return PAE.getCourses();
+    }
+
+    @ModelAttribute(name = "course")
+    public Course course(Model model) {
+        return new Course();
+    }
+
     @GetMapping("/courses")
-    public String courses(Model model) {
-        model.addAttribute("courses", PAE.getCourses());
-        model.addAttribute("course", new Course());
+    public String home(Model model) {
         return "courses";
     }
 
@@ -23,9 +37,11 @@ public class CourseController {
     }
 
     @PostMapping("/course/create")
-    public String create(Course course) {
-        PAE.addCourse(course);
-        return "home";
+    public String create(@Valid Course course, Errors errors) {
+        if (!errors.hasErrors()){
+            PAE.addCourse(course);
+        }
+        return "redirect:/courses";
     }
 
 }
