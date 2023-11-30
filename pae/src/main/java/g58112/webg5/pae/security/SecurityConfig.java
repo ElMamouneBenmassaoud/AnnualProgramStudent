1465fbfc09fbd6000e2086d17b0aca23274ab591
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-11-30 18:02:36 +0100

    inserer les users dans data.sql

--- pae/src/main/java/g58112/webg5/pae/security/SecurityConfig.java
+++ pae/src/main/java/g58112/webg5/pae/security/SecurityConfig.java
@@ -3,17 +3,22 @@ package g58112.webg5.pae.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -47,7 +52,7 @@ public class SecurityConfig {
         http.formLogin(form -> form
                 .loginPage("/login")
                 .permitAll()
-                .defaultSuccessUrl("/home"));
+                .defaultSuccessUrl("/"));
         http.authorizeHttpRequests((authz) -> authz
                 .requestMatchers(mvc.pattern("/courses/**")).authenticated()
                 .requestMatchers(mvc.pattern("/students/**")).authenticated()
